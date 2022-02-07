cb468cec5ac1055fef70932e31ba4998911e8db0
悠二 <youer.xyc@alibaba-inc.com>
2022-02-07 17:48:05 +0800

    feature：添加logger作为初始化的测试库

--- app/src/main/java/com/youer/androiskilltree/App.java
+++ app/src/main/java/com/youer/androiskilltree/App.java
@@ -2,6 +2,10 @@ package com.youer.androiskilltree;
 
 import android.app.Application;
 import android.content.Context;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
 import com.youer.reflection.Hook;
 
 /**
@@ -10,8 +14,25 @@ import com.youer.reflection.Hook;
  */
 public class App extends Application {
     @Override
+    public void onCreate() {
+        super.onCreate();
+        initLogger();
+    }
+
+    private void initLogger() {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(false)
+            .tag("AndroidSkillTree")
+            .build();
+
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
+    }
+
+    @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+        // hook相关的事件
         Hook.hookPackageManager(base);
+
     }
 }
